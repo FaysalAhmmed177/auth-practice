bc1b8edfecfef9755d92f479f89c42e045b9b18b
Fayal Ahmmed <faysalahmmed177@gmail.com>
2021-03-18 22:24:36 +0600

    firebase authentication added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,160 @@
 import logo from './logo.svg';
 import './App.css';
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+import { useState } from 'react';
+
+if (firebase.apps.length === 0) {
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+}
 
 function App() {
+  const [newUser, setNewUser] = useState(false);
+  const [user, setUser] = useState({
+    isSignedIn: false,
+    newUser: false,
+    name: '',
+    email: '',
+    password: '',
+    photo: '',
+    error: '',
+    success: false
+  })
+  const googleProvider = new firebase.auth.GoogleAuthProvider();
+  const githubProvider = new firebase.auth.GithubAuthProvider();
+
+  const handleGoogleSignIn = () => {
+    firebase.auth()
+      .signInWithPopup(googleProvider)
+      .then((result) => {
+        const { displayName, photoURL, email } = result.user;
+        const signedInUser = {
+          isSignedIn: true,
+          name: displayName,
+          email: email,
+          photo: photoURL
+        };
+        setUser(signedInUser);
+      }).catch((error) => {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        var email = error.email;
+        console.log(errorCode, errorMessage, email);
+      });
+  };
+
+  const handleGithubSignIn = () => {
+    firebase
+      .auth()
+      .signInWithPopup(githubProvider)
+      .then((result) => {
+        var user = result.user;
+        setUser(user);
+
+      }).catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        var email = error.email;
+        console.log(errorCode, errorMessage, email);
+      });
+  }
+
+  //set new user name, email and password into state
+  const handleBlur = (e) => {
+    let isFieldValid = true;
+    if (e.target.name === 'email') {
+      isFieldValid = /\S+@\S+\.\S+/.test(e.target.value);
+
+    }
+
+    if (e.target.name === 'password') {
+      const isPasswordValid = e.target.value.length > 6;
+      const passwordHasNumber = /\d{2}/.test(e.target.value);
+      isFieldValid = isPasswordValid && passwordHasNumber;
+    }
+
+    if (isFieldValid) {
+      const newUserInfo = { ...user };
+      newUserInfo[e.target.name] = e.target.value;
+      setUser(newUserInfo);
+    }
+  }
+
+  //createUserWithEmailAndPassword
+  const handleSubmit = (e) => {
+    console.log(user.email, user.password);
+    if (newUser && user.email && user.password) {
+      firebase.auth().createUserWithEmailAndPassword(user.email, user.password)
+        .then((res) => {
+          const newUserInfo = { ...user };
+          newUserInfo.error = '';
+          newUserInfo.success = true;
+          setUser(newUserInfo);
+          updateUserName(user.name);
+        })
+        .catch((error) => {
+          const newUserInfo = { ...user };
+          newUserInfo.error = error.message;
+          newUserInfo.success = false;
+          setUser(newUserInfo);
+        });
+    }
+
+    if (!newUser && user.email && user.password) {
+      firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+        .then((res) => {
+          // Signed in
+          //var user = res.user;
+          const newUserInfo = { ...user };
+          newUserInfo.error = '';
+          newUserInfo.success = true;
+          setUser(newUserInfo);
+          console.log('sign in user info', res.user);
+        })
+        .catch((error) => {
+          const newUserInfo = { ...user };
+          newUserInfo.error = error.message;
+          newUserInfo.success = false;
+          setUser(newUserInfo);
+        });
+    }
+
+    e.preventDefault();
+  }
+//update user name
+  const updateUserName = name => {
+    var user = firebase.auth().currentUser;
+
+    user.updateProfile({
+      displayName: name,
+      photoURL: "https://example.com/jane-q-user/profile.jpg"
+    }).then(function () {
+      // Update successful.
+      console.log('User name updated successfully');
+    }).catch(function (error) {
+      // An error happened.
+      console.log(error);
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Sign up for your account</p>
+      <input type="checkbox" onChange={() => setNewUser(!newUser)} name="" id="" />
+      <label htmlFor="newUser">New user sign up</label>
+      <form onSubmit={handleSubmit}>
+        {newUser && <input name="name" type="text" onBlur={handleBlur} placeholder="Enter your name" />}<br />
+        <input type="text" name="email" onBlur={handleBlur} placeholder="Enter your email" required /><br />
+        <input type="password" name="password" onBlur={handleBlur} placeholder="Enter your password" required /><br />
+        <input type="submit" value={newUser? 'Sign up' : 'Sign in'} />
+      </form><br />
+      <p style={{ color: 'red' }}>{user.error}</p>
+      {user.success && <p style={{ color: 'green' }}>User {newUser ? 'created' : 'logged in'} successfully</p>}
+      <button style={{ backgroundColor: 'red', color: 'white' }} onClick={handleGoogleSignIn}>Sign in using Google</button><br />
+      <button style={{ backgroundColor: 'black', color: 'white' }} onClick={handleGithubSignIn}>Sign in using Github</button><br />
     </div>
   );
 }
