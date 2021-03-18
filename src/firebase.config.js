5eca29eddbee476bf80d26587e7f74a6b3855633
Fayal Ahmmed <faysalahmmed177@gmail.com>
2021-03-18 11:16:11 +0600

    create app for auth-practice

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA5rTIYMwgO6Ir_LpTl-73VhvKtLoMG8C4",
+    authDomain: "auth-practice-e6ce5.firebaseapp.com",
+    projectId: "auth-practice-e6ce5",
+    storageBucket: "auth-practice-e6ce5.appspot.com",
+    messagingSenderId: "869141420620",
+    appId: "1:869141420620:web:b7ff007be4c9242c7a32e9"
+  };
+
+  export default firebaseConfig;
